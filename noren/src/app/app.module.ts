79a36e82332c8df254732b6bcff17129f01fe148
Anna-12-Mer <mammeramna@gmail.Com>
2020-12-05 16:25:20 +0100

    add components

--- noren/src/app/app.module.ts
+++ noren/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomePageComponent } from './home-page/home-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule
